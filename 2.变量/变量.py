ef37bd149869efccf07198bb7f01ddf3305ec504
loveniuzi <46342410+loveniuzi@users.noreply.github.com>
2019-10-18 18:08:02 +0800

    Create 变量.py

---
+++ 2.变量/变量.py
@@ -0,0 +1,22 @@
+#1输出变量
+#让用户猜一个数字，将用户输入的数字打印出来
+num = input("请猜一个数：")
+print("你的输入是：" + num)
+
+#2变量计算
+money = 20
+
+# 将 money 变量的值乘以 2 再加上 50
+# 赋值给 result
+result = money * 2 + 50
+
+print(result)
+
+#3. 变量类型
+#想一想以下变量分别是什么。输出结果，看看是否符合你的判断
+a = 'hello,world'
+b = 1.414
+c = (1 != 2)
+d = (True and False)
+e = (True or False)
+print(a, b, c, d, e)
