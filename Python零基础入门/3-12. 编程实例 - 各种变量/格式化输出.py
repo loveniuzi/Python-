62cc986c4214c8beb8136810dba9f78daf25b8c8
loveniuzi <46342410+loveniuzi@users.noreply.github.com>
2019-10-18 17:53:25 +0800

    Create 格式化输出.py

---
+++ Python零基础入门/3-12. 编程实例 - 各种变量/格式化输出.py
@@ -0,0 +1,16 @@
+#问题1
+a = "hello,world"
+b = 1.414
+c = (1 != 2)
+d = (True and False)
+e = (True or False)
+
+print(a, b, c, d, e)
+
+
+#问题2
+print("a:%s;b:%.2f;c:%s;d:%s;e:%s;"%(a, b, c, d, e))
+
+
+#问题3
+print("\'hello,world\'\n\\\"hello,world\"")
