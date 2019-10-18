f427eb9249112f7da5f89f9063ecd8c69cad4311
loveniuzi <46342410+loveniuzi@users.noreply.github.com>
2019-10-18 10:10:53 +0800

    Create 2-5.py
    【问题描述】
    打印自己的英文昵称
    打印一个含有加减乘除的数学表达式
    使用两次 print 语句，但只显示一行
    使用一次 print 语句，但显示在两行中

---
+++ Python零基础入门/2-5. 编程实例 - 更多的输出/2-5.py
@@ -0,0 +1,9 @@
+#打印自己的英文昵称
+print("wang zhe")
+#打印一个含有加减乘除的数学表达式
+print("4 + 5 * 8 / 4 =", 4 + 5 * 8 / 4 )
+#使用两次 print 语句，但只显示一行
+print("Hello,", end = "")
+print("World!")
+#使用一次 print 语句，但显示在两行中
+print("Hello \nWorld!")
