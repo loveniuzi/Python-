061346e8738dfe1dda0c8af67b658d091cb3b825
loveniuzi <46342410+loveniuzi@users.noreply.github.com>
2019-10-22 17:39:41 +0800

    Create 2逻辑判断.py

---
+++ Python纯零基础入门小组/4.If语句/2逻辑判断.py
@@ -0,0 +1,31 @@
+#a组
+bingo = False
+if bingo == False:
+    print(True)
+else:
+    print(False)
+
+#b组
+b = 3
+if b - 3:
+    print(True)
+else:
+    print(False)
+
+#c组
+x = input('请输入内容')
+if x:
+    print(True)
+else:
+    print(False)
+
+#d组
+a = True
+b = not a  # 不记得not请回顾第二课
+print(b)
+print(not b)
+print(a == b)
+print(a != b)
+print(a and b)
+print(a or b)
+print(1<2 and b==True)
