f205e54dcdd4cbf71ade3f27dc904ad3646a1ac9
loveniuzi <46342410+loveniuzi@users.noreply.github.com>
2019-11-12 17:17:09 +0800

    Update 判断质数.py

--- 刷题100小组/25判断质数/判断质数.py
+++ 刷题100小组/25判断质数/判断质数.py
@@ -2,12 +2,15 @@
 # 【分析】一个大于1的自然数，除了1和它本身外，不能被其他自然数（质数）整除（2, 3, 5, 7等），
 # 换句话说就是该数除了1和它本身以外不再有其他的因数.
 number = int(input("请输入一个正整数:"))
-i = 1
-while i != number:
-    if (number % i) != 0:
-        print(f"{number}不是质数")
-        break
-    else:
-        i = i + 1
-if i == number:
-    print(f"{number}是质数")
+if number > 1:
+    i = 2
+    while i != number:
+        if (number % i) != 0:
+            print(f"{number}不是质数")
+            break
+        else:
+            i = i + 1
+    if i == number:
+        print(f"{number}是质数")
+else:
+    print(f"{number}不是质数")
