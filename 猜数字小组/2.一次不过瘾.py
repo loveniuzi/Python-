f24e003667611c5fa70082c08aa98e2a312c6af3
loveniuzi <46342410+loveniuzi@users.noreply.github.com>
2019-12-01 09:32:31 +0800

    Create 2.一次不过瘾.py

---
+++ 猜数字小组/2.一次不过瘾.py
@@ -0,0 +1,19 @@
+##程序在用户猜中答案后，输出猜中答案一共猜了多少轮（用户每输入一次计做一轮），
+##并可以反复进行游戏（用户猜中一次后可选择“继续”还是“退出”）
+
+import random
+
+secret = random.randint(1, 100)
+count = 0
+playAgain = 'y'
+
+while playAgain == 'y':
+    guess = int(input("请猜一个1到100之间的整数:"))
+    count = count + 1
+    if guess > secret:
+        print("你猜的数字大了")
+    elif guess < secret:
+        print("你猜的数字小了")
+    else:
+        print(f"恭喜你！猜对了数字是:{secret},你一共猜了{count}次。")
+        playAgain = input("你是否还要继续游戏?(继续输入y)").lower()
